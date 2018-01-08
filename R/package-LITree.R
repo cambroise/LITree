03290d3204a347ddd4a129522394e2b8092d8d18
cambroise <christophe.ambroise@gmail.com>
2018-01-08 16:37:33 +0100

    help of the package

--- R/package-LITree.R
+++ R/package-LITree.R
@@ -1,10 +1,8 @@
 #' LITree: A package for infering Gaussian Graphical Models with unobserved variables using latent tree agreggation
 #'
-#' @section Classes
 #'
 #' The LITree package provides four classes  graphModel, GGMmodel, GGMfit,  GGMexperiment and one function em.litree()
 #'
-#'  TODO
 #' @author Christophe Ambroise \email{christophe.ambroise@@univ-evry.fr}
 #' @author Geneviève Robin \email{genevieve.robin@@polytechnique.edu}
 #' @author Stéphane Robin \email{stéphane.robin@@agroparistech.edu}
